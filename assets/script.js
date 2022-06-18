f499dfc0ea8747aa457cc4c77feda26d090a5542
Nick Zabkar <nzabkar51@gmail.com>
2022-06-18 15:20:59 -0400

    Rendered API data to cards.

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-
 const APIKey = 'Z6zdp7ZT2n5gT0GoFhtSflRhfb02qkTZecfzsVaS';
 
 const getApiData = () => {
@@ -15,12 +14,47 @@ const getApiData = () => {
 }
 
 const displayPark = (parkData) => {
-    document.getElementById('park-name').innerHTML = parkData.data[0].fullName;
-    document.getElementById('park-description').innerHTML = parkData.data[0].description;
-    document.getElementById('park-image').setAttribute("src", parkData.data[0].images[0].url);
-    document.getElementById('park-coordinates').innerHTML = parkData.data[0].latLong;
-    document.getElementById('park-activities').innerHTML = parkData.data[0].activities[0].name;
-    document.getElementById('park-links').setAttribute("href", parkData.data[0].url);
+    console.log(parkData);
+    const cardsContainer = document.querySelector(".cards-container");
+    parkData.data.forEach(park => {
+        console.log(park);
+        const {activities, description, images, name, latLong, url} = park;
+        const cardBody = document.createElement("article");
+        const parkName = document.createElement("h2");
+        const parkDescription = document.createElement("p");
+        const parkImage = document.createElement("img");
+        const coordinatesTitle = document.createElement("h3");
+        const coordinates = document.createElement("p");
+        const activitiesDiv = document.createElement("div");
+        const parkLink = document.createElement("a");
+        
+        parkName.innerHTML = name;
+        parkDescription.innerHTML = description;
+        coordinates.innerHTML = latLong;
+        parkLink.innerHTML = "NPS Link";
+
+        parkImage.src = images[0].url;
+        parkLink.href = url;
+
+        cardBody.classList.add("cardBody");
+        parkImage.classList.add("park-image");
+
+        activities.forEach((activity, i) => {
+            if (i<=2) {const p = document.createElement("p");
+                p.innerHTML = activity.name;
+                activitiesDiv.appendChild(p);}
+        });
+
+        cardBody.appendChild(parkName);
+        cardBody.appendChild(parkDescription);
+        cardBody.appendChild(parkImage);
+        cardBody.appendChild(coordinatesTitle);
+        cardBody.appendChild(coordinates);
+        cardBody.appendChild(activitiesDiv);
+        cardBody.appendChild(parkLink);
+        cardsContainer.appendChild(cardBody);
+
+    });
 }
 
 getApiData();
